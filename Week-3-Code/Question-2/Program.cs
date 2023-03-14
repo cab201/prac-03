d3339098cdfa44b7323993f872e620fe38422cb1
unknown <MeltUser01@qut.edu.au>
2023-03-15 09:49:56 +1000

    Added question 1 and 2

---
+++ Week-3-Code/Question-2/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Question_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int limit;
+
+            // Asks the user to enter the limit, until we get
+            // a positive, whole number
+            Console.WriteLine("Enter the limit: ");
+            string input = Console.ReadLine();
+            while (!(int.TryParse(input, out limit) 
+                && limit > 0))
+            {
+                Console.WriteLine
+                    ("The limit must be a positive whole number");
+                input = Console.ReadLine();
+            }
+
+            int[] arrayOfInts = new int[limit];
+
+            for (int i = 0; i < limit; i++)
+            {
+                Console.WriteLine
+                    ($"Enter number at position {i}: ");
+                input = Console.ReadLine();
+
+                // Ask for input, until we get a whole number (int)
+                int number;
+                while(!int.TryParse(input, out number))
+                {
+                    Console.WriteLine("Please enter a whole number");
+                    input = Console.ReadLine();
+                }
+
+                // Set element in array to the number entered by user
+                arrayOfInts[i] = number;
+            }
+
+            // Display the elements:
+            foreach (int num in arrayOfInts)
+            {
+                Console.Write("{0}, ", num);
+            }
+
+            // Calculate and display the total
+            int cumulativeSum = 0;
+            foreach (int num in arrayOfInts)
+            {
+                // Equivalent to cumulativeSum = cumulativeSum + num;
+                cumulativeSum += num;
+            }
+            Console.WriteLine("The total is {0}", 
+                cumulativeSum);
+
+            // Calculate and display the average
+            Console.WriteLine("The average is {0}", 
+                (double) cumulativeSum / limit);
+        }
+    }
+}
---
+++ Week-3-Code/Week-3-Code/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Week_3_Code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.	Write a program called ArrayDouble
+            //which stores an array of 5 doubles and
+            //prints them out. The numbers in the array
+            //should be hard coded.
+            //You should use an array and a for loop.
+
+            double[] arrayOfDoubles = new double[5]
+            {
+                1.0, 2.0, 3.0, 4.0, 5.0
+            };
+
+            for (int index = 0;
+                index < arrayOfDoubles.Length;
+                index++)
+            {
+                Console.Write(arrayOfDoubles[index] + ", ");
+            }
+        }
+    }
+}
